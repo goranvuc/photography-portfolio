9895abde35bc613aa02bde89273c7b2f556025b6
goranvuc <141842571+goranvuc@users.noreply.github.com>
2026-08-11 05:43:15 +0200

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/photography-portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
